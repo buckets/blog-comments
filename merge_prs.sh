25b497f3aeb42b57986ac231adf671d058116ee7
Matt Haggard <haggardii@gmail.com>
2021-11-03 11:03:36 -0400

    Add script to automate comment merging

---
+++ merge_prs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Before running this, close the PRs you don't want merged."
+read -p "Press enter to continue..."
+
+if ! git remote | grep bot; then
+  git remote add bot https://github.com/buckets-comments/blog-comments.git
+fi
+
+git fetch --prune bot >/dev/null 2>/dev/null
+for branch in $(git branch --remotes --no-merged); do
+  echo "-------------------------------------------------------"
+  echo "$branch"
+  git diff ...${branch}
+  read -p "Press enter to keep. Type 'n' (or anything) to skip: " answer
+  if [ -z "$answer" ]; then
+    git merge --no-edit "${branch}"
+  else
+    echo skipping ${branch}
+  fi
+done
