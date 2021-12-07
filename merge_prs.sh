70fa499f09cc8106d77a11071feb5588978fdbb1
Matt Haggard <haggardii@gmail.com>
2021-12-07 10:09:41 -0500

    Cleanup merged branches

--- merge_prs.sh
+++ merge_prs.sh
@@ -9,10 +9,11 @@ git fetch --prune bot >/dev/null 2>/dev/null
 for branch in $(git branch --remotes --no-merged); do
   echo "-------------------------------------------------------"
   echo "$branch"
+  shortname="$(echo "$branch" | cut -d/ -f2-)"
   git diff ...${branch}
   read -p "Press enter to keep. Type 'n' (or anything) to skip: " answer
   if [ -z "$answer" ]; then
-    git merge --no-edit "${branch}"
+    git merge --no-edit "${branch}" && git push bot ":$shortname"
   else
     echo skipping ${branch}
     skipped="${skipped} ${branch}"
@@ -24,7 +25,8 @@ if ! [ -z "$skipped" ]; then
   echo "To delete comments (username: buckets-comments):"
   echo ""
   for branch in $skipped; do
-    echo "git push bot ':$branch'"
+    shortname="$(echo "$branch" | cut -d/ -f2-)"
+    echo "git push bot ':$shortname'"
   done
   echo ""
 fi
